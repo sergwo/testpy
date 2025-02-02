18ac9f5c45edd04528c13e5cc0b4def7b088e2bf
Serg <35146810+sergwo@users.noreply.github.com>
2025-02-02 17:45:48 +0300

    Лабораторная работа 3
    ООП

---
+++ Лабораторная работа 3/task.py
@@ -0,0 +1,55 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+    @property
+    def name(self):
+        return self._name
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages):
+        if not isinstance(new_pages, int) or new_pages <= 0:
+            raise ValueError("Количество страниц должно быть целым числом больше 0.")
+        self._pages = new_pages
+
+    def __str__(self):
+        return f"{super().__str__()}. Страниц в бумажной книге: {self.pages}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+    @duration.setter
+    def duration(self, new_duration):
+        if not isinstance(new_duration, (int,float)) or new_duration <= 0:
+            raise ValueError ("Длительность аудиокниги должна быть числом болше 0")
+        self._duration = new_duration
+
+
+    def __str__(self):
+        return f" {super().__str__()}. Продолжительность аудиокниги: {self.duration} ч."
